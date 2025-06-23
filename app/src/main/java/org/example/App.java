e5b8f9af5c9781356cf6ea456ea7e4475c763657
Henil1601 <henilp1601@gmail.com>
2025-06-22 19:09:43 -0500

    completed Shape Your Destiny lab with all classes and tests

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("You can write a driver program for your code here!");
-  }
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+        Shape triangle = new RightTriangle(3, 4);
+        Shape square = new Square(4);
+        Shape isoTriangle = new IsoscelesRightTriangle(5);
+
+        System.out.println("Circle Area: " + circle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+        System.out.println("Triangle Area: " + triangle.getArea());
+        System.out.println("Square Perimeter: " + square.getPerimeter());
+        System.out.println("Isosceles Triangle Area: " + isoTriangle.getArea());
+    }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -4,9 +4,38 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  // You can write your unit tests here.
-  @Test
-  void appWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void testCircle() {
+        Circle c = new Circle(5);
+        assertEquals(Math.PI * 25, c.getArea(), 0.01);
+        assertEquals(2 * Math.PI * 5, c.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testRectangle() {
+        Rectangle r = new Rectangle(4, 6);
+        assertEquals(24, r.getArea());
+        assertEquals(20, r.getPerimeter());
+    }
+
+    @Test
+    void testRightTriangle() {
+        RightTriangle rt = new RightTriangle(3, 4);
+        assertEquals(6, rt.getArea());
+        assertEquals(12, rt.getPerimeter(), 0.01);
+    }
+
+    @Test
+    void testSquare() {
+        Square s = new Square(4);
+        assertEquals(16, s.getArea());
+        assertEquals(16, s.getPerimeter());
+    }
+
+    @Test
+    void testIsoscelesRightTriangle() {
+        IsoscelesRightTriangle iso = new IsoscelesRightTriangle(5);
+        assertEquals(12.5, iso.getArea());
+        assertEquals(5 + 5 + Math.sqrt(50), iso.getPerimeter(), 0.01);
+    }
 }
